4c396880e77bcb5df2beb7087563c13d3f4ad1c1
Rob Peck <rob@robpeck.com>
2017-01-16 21:49:00 -0600

    Create a seeder to create admin user.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        if ($this->command->confirm('Do you wish to refresh migration before seeding, it will clear all old data ?')) {
+
+            // Call the php artisan migrate:refresh using Artisan
+            $this->command->call('migrate:refresh');
+            $this->command->line("Data cleared, starting from blank database.");
+        }
+
+        $email = $this->command->ask('What would you like your initial admin account email to be?');
+        $name = $this->command->ask('What would you like your initial admin account name to be?');
+        $password  = $this->command->ask('What would you like your initial admin account password to be?');
+
+        $user = new User;
+        $user->email = $email;
+        $user->name = $name;
+        $user->password = bcrypt($password);
+        $user->approved = 1;
+        $user->can_upload = 1;
+        $user->can_manage = 1;
+        $user->can_admin = 1;
+
+        if ($user->save()) {
+            $this->command->line("Admin user created.");
+        }
     }
 }
